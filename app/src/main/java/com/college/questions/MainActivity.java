7dcce32791e242b23dda3b434f73a24a07e6cfa5
Chris Paz Ryc <chris.paz.ryc@gmail.com>
2024-01-30 02:29:33 -0500

    Add getOperand functions and addOperand function

--- app/src/main/java/com/college/questions/MainActivity.java
+++ app/src/main/java/com/college/questions/MainActivity.java
@@ -2,6 +2,11 @@ package com.college.questions;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 /*
  TODO:
@@ -19,4 +24,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    int getOperandOne() {
+        EditText op1 = findViewById(R.id.operand_one_edit_1);
+        return Integer.parseInt(op1.getText().toString());
+    }
+
+    int getOperandTwo() {
+        EditText op2 = findViewById(R.id.operand_two_edit_2);
+        return Integer.parseInt(op2.getText().toString());
+    }
+
+    public void addOperands(View view) {
+        int result = getOperandOne() + getOperandTwo();
+        TextView resultBox = findViewById(R.id.operation_result);
+        resultBox.setText(String.format(Locale.US, "%d", result));
+    }
 }
